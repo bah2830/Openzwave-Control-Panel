bb4aff5b9f5dfc97770feac6847030793f5ae592
Brent Hughes <brentahughes@gmail.com>
2017-03-19 11:32:37 -0500

    Updated script

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Set the network key if supplied
 if [ -n "$NETWORK_KEY" ]; then
     sed -i 's/\(NetworkKey" value="\).*/\1'"$NETWORK_KEY"'" \/>/g' /app/Openzwave-control-panel/config/options.xml
